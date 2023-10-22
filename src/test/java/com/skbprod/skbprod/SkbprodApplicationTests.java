8758c540729e5bb45925e4e98d8dd88e74a6af90
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-10-22 19:57:06 -0300

    Initial commit

---
+++ src/test/java/com/skbprod/skbprod/SkbprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skbprod.skbprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
